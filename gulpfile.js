0c1ec41f9e11e7024cea30a55c1d4c7b49e70322
3Z0Z <ezozplayer11@gmail.com>
2023-03-23 16:49:59 -0500

    actualizacion 23/03/2023

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ function versionWebp() {
     const opciones = {
         quality: 50
     }
-    return src('src/img/**/*.{png,jpg}')
+    return src('src/img/**/*.{png,jpg,jpeg}')
         .pipe( webp( opciones ) )
         .pipe( dest('build/img') )
 }
@@ -40,7 +40,7 @@ function versionAvif() {
     const opciones = {
         quality: 50
     }
-    return src('src/img/**/*.{png,jpg}')
+    return src('src/img/**/*.{png,jpg,jpeg}')
         .pipe( avif( opciones ) )
         .pipe( dest('build/img'))
 }
